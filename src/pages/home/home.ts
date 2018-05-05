bc49d7558b137c870d0f423b6c9642ea5c123547
Farias1785 <gabrielfarias85@gmail.com>
2018-05-05 14:27:11 -0300

    Disable street view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,53 +1,3 @@
-// import { Component ,ViewChild, ElementRef } from '@angular/core';
-// import { NavController,ToastController } from 'ionic-angular';
-// import { AngularFireAuth } from "angularfire2/auth";
-
-// declare var google;
-
-// @Component({
-//   selector: 'page-home',
-//   templateUrl: 'home.html'
-// })
-// export class HomePage {
-
-//   @ViewChild('map') mapElement: ElementRef;
-//   map: any;
-
-//   constructor(private afAuth:AngularFireAuth,private toast:ToastController,public navCtrl: NavController) {
-
-//   }
-//   ionViewDidLoad(){
-//     this.loadMap();
-//     this.afAuth.authState.subscribe(data => {
-//       if (data && data.email && data.uid){
-//          this.toast.create({
-//             message:`Bem-vindo,${data.email}`,
-//             duration:3000
-//           }).present();
-//        }else{
-//         this.toast.create({
-//           message:`usuario não encontrado`,
-//           duration:3000
-//         }).present();
-//       }
-//     });
-//   }
-
-//   loadMap(){
-    
-//        let latLng = new google.maps.LatLng(-34.9290, 138.6010);
-    
-//        let mapOptions = {
-//          center: latLng,
-//          zoom: 15,
-//          mapTypeId: google.maps.MapTypeId.ROADMAP
-//        }
-    
-//        this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-    
-//      }
-
-// }
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController,ToastController } from 'ionic-angular';
 import { AngularFireAuth } from "angularfire2/auth"
@@ -92,7 +42,8 @@ export class HomePage {
     let mapOptions = {
       center: latLng,
       zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      streetViewControl:false,
+      mapTypeId: 'roadmap'
     }
  
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
