2da336ffafbc5c7535e1d80cbe5c0a6027d9593b
CosmoSantos <cosmosantos13@gmail.com>
2018-05-05 20:54:20 -0300

    Pagina veiculo

--- src/pages/veiculo/veiculo.ts
+++ src/pages/veiculo/veiculo.ts
@@ -1,5 +1,5 @@
-import { Component, NgModule } from '@angular/core';
-import { IonicPage, NavController, NavParams, Alert } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { Veiculo } from '../../models/veiculo';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { FormsModule } from '@angular/forms';
@@ -13,19 +13,16 @@ export class VeiculoPage {
 
   veiculo = {} as Veiculo;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private db: AngularFireDatabase, private form: FormsModule) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, 
+    private db: AngularFireDatabase, private form: FormsModule, private alertCtrl: AlertController) {
       
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad VeiculoPage');
   }
 
   cadastrar(veiculo: Veiculo) {
       try {
         if(veiculo.marca !== null && veiculo.marca !== undefined) {
           this.db.list('Veiculo').push(this.veiculo);
-          alert(">>>Veiculo Cadastrado com sucesso <<< " + veiculo.marca)
+          this.showAlert();
         } else {
           alert("Veiculo com valor nulo: " + veiculo);
         }
@@ -34,6 +31,15 @@ export class VeiculoPage {
         alert(">>> Erro ao cadastrar veiculo <<<");
       }
 
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Veiculo',
+      subTitle: 'Veiculo ' + this.veiculo.modelo + " cadastrado com sucesso.",
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
 }
