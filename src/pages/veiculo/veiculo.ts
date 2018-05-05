531370ba42a53eebdb77540e5029be794b6b8128
CosmoSantos <cosmosantos13@gmail.com>
2018-05-04 23:51:37 -0300

    Atualizacoes pagina Veiculo

--- src/pages/veiculo/veiculo.ts
+++ src/pages/veiculo/veiculo.ts
@@ -1,24 +1,21 @@
-import { Component, Inject, Injectable } from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database';
-
 
 @IonicPage()
 @Component({
   selector: 'page-veiculo',
   templateUrl: 'veiculo.html',
 })
-@Injectable()
-export class VeiculoPage {
 
-  private PATH = 'veiculo/';
+export class VeiculoPage {
 
   veiculo = {
     marca: 'Chevrolet',
     modelo: 'S10',
-    cor: 'Preta'
+    cor: 'Preta',
+    anoFabricacao: '2018'
   }
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private  db: AngularFireDatabase) {
@@ -30,7 +27,13 @@ export class VeiculoPage {
   }
 
   save(veiculo: any) {
-
+    try {
+      if(veiculo !== null) {
+        
+      }
+    } catch(e) {
+      console.log(e);
+    }
   }
 
 }
